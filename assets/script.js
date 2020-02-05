a5ec283e1936b1125202e8f15c3ecb95b0d0886e
AJD <adriana.j.didden@gmail.com>
2020-02-05 10:50:55 -0500

    created assets folder, reset css file and css folder

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var quizContainer = document.getElementsByClassName("quiz");
+var resultsContainer = document.getElementsByClassName("results");
+var submitBTN = document.getElementById("submit");
+var questions = [
+    {
+        question: "1. Commonly used data types DO NOT include",
+        answers: ['string','booleans','alerts','numbers'],
+        correctAnswers: "3"
+    },
+
+    {
+        question: "2. where is the correct place to insert a javaScript?",
+        answers: ['<head>','<body>','<img>','<div>' ],
+        correctAnswers: "2"
+    }
+];
+
+
+//timed quiz on javascript fundamentals 
+//stores high scores so i can gauge my progress to my peers
+
+//click start buttom
+    //timer begins and presented a question
+
+    //when answer is clicked, the next question appears
+
+    //if question is answered incorrectly time is subtracted from timer
+
+    //if all questions are completed OR the timer = 0 the quiz is over
+
+    //when the quiz is over i can save my initials and score
+
+
+
+    //submit URL of github and functional deployed version
+
+    //add readme!
